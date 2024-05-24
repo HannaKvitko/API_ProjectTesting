c71dfdd96e51e3f8d07a981df9787dde17ba21f6
khudoyarovaaziza <153706206+khudoyarovaaziza@users.noreply.github.com>
2024-05-23 22:52:55 -0400

    finished check it)))

---
+++ src/test/java/POJO_Models/Student.java
@@ -0,0 +1,26 @@
+package POJO_Models;
+
+public class Student {
+    private String id;
+
+    public Student() {}
+
+    public Student(String id) {
+        this.id = id;
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                '}';
+    }}
